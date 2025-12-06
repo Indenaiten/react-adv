0c6211b737b576e3dadfb66f2d19022b4db5cefc
Ángel Herce Soto <indenaiten@gmail.com>
2025-12-06 08:42:21 +0100

    fix: se han corregido cosas del ejercicio anterior

--- src/component-patterns/components/ProductCard.tsx
+++ src/component-patterns/components/ProductCard.tsx
@@ -6,21 +6,22 @@ export const ProductCard = () => {
 
     const [ counter, setCounter ] = useState(0);
     
-    const increment = () => setCounter( counter + 1 );
-    const decrement = () => setCounter( counter > 0 ? counter - 1 : counter );
+    const increment = ( value: number ) => setCounter( prev => Math.max( 0, prev + value ));
+    const decrement = ( value: number ) => setCounter( prev => Math.max( 0, prev - value ));
 
     return (
         <div className={ styles.productCard }>
             <img className={styles.productImg} src="./coffee-mug.png" alt="Coffe Mug"/>
             {/*<img className={styles.productImg} src={noImage} alt="Image not found"/>*/}
+
             <span className={styles.productDescription}>Coffee Mug</span>
 
             <div className={styles.buttonsContainer}>
-                <button className={styles.buttonMinus} onClick={decrement}>-</button>
+                <button className={styles.buttonMinus} onClick={() => decrement( 1 )}>-</button>
 
                 <div className={styles.countLabel}>{counter}</div>
 
-                <button className={styles.buttonAdd} onClick={increment}>+</button>
+                <button className={styles.buttonAdd} onClick={() => increment( 1 )}>+</button>
             </div>
         </div>
     )
