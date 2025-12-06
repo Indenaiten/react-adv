88b659cf90cd5cdbf38f3863d53cdbf34416d442
Ángel Herce Soto <indenaiten@gmail.com>
2025-12-06 08:59:28 +0100

    feat: se ha creado un componente de producto

--- src/component-patterns/components/ProductCard.tsx
+++ src/component-patterns/components/ProductCard.tsx
@@ -1,18 +1,22 @@
 import styles from '../styles/styles.module.css';
-import noImage from '../assets/no-image.jpg';
-import { use, useState } from 'react';
 import { useProductCounter } from '../hooks/ProductCounterHook';
+import { Product } from '../model';
+import noImage from '../assets/no-image.jpg';
 
-export const ProductCard = () => {
+type props = {
+    product: Product;
+}
+
+export const ProductCard = ({ product }: props ) => {
 
     const { counter, increment, decrement } = useProductCounter({});
 
     return (
         <div className={ styles.productCard }>
-            <img className={styles.productImg} src="./coffee-mug.png" alt="Coffe Mug"/>
+            <img className={styles.productImg} src={ product.img ? product.img : noImage } alt={ product.img ? product.title : 'Image not found' }/>
             {/*<img className={styles.productImg} src={noImage} alt="Image not found"/>*/}
 
-            <span className={styles.productDescription}>Coffee Mug</span>
+            <span className={styles.productDescription}>{ product.title }</span>
 
             <div className={styles.buttonsContainer}>
                 <button className={styles.buttonMinus} onClick={() => decrement( 1 )}>-</button>
--- src/component-patterns/pages/ShoppingPage.tsx
+++ src/component-patterns/pages/ShoppingPage.tsx
@@ -1,11 +1,27 @@
+import { P } from "react-router/dist/development/instrumentation-BB0wRuqz"
 import { ProductCard } from "../components/ProductCard"
+import { Product } from "../model"
+
+const products: Product[] = [
+    { id: '1', title: 'Coffee Mug', img: './coffee-mug.png' },
+    { id: '2', title: 'Test' },
+    { id: '3', title: 'Test' },
+    { id: '4', title: 'Test' },
+    { id: '5', title: 'Test' },
+    { id: '6', title: 'Test' },
+    { id: '7', title: 'Test' },
+    { id: '8', title: 'Test' },
+]
 
 export const ShoppingPage = () => {
     return (
         <div>
             <h1>Shopping Page</h1>
             <hr />
-            <ProductCard />
+            
+            <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: 10 }}>
+                { products.map( product => ( <ProductCard key={product.id} product={product} /> )) }
+            </div>
         </div>
     )
 }
