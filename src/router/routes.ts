dea4feb2b8b2195fe67b9a432596c542e2076e2d
Ángel Herce Soto <indenaiten@gmail.com>
2025-12-06 07:18:08 +0100

    feat: se ha renombrado los chunks con webpack

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -10,7 +10,7 @@ interface Route {
 }
 
 export const routes: Route[] = [
-    { to: '/lazy/1', path: 'lazy/1', component: lazy(() => import( '../01-lazyload/pages/LazyPage1' )), name: 'Lazy 1' },
-    { to: '/lazy/2', path: 'lazy/2', component: lazy(() => import( '../01-lazyload/pages/LazyPage2' )), name: 'Lazy 2' },
-    { to: '/lazy/3', path: 'lazy/3', component: lazy(() => import( '../01-lazyload/pages/LazyPage3' )), name: 'Lazy 3' },
+    { to: '/lazy/1', path: 'lazy/1', component: lazy(() => import( /* webpackChunkName: "LazyPage1" */ '../01-lazyload/pages/LazyPage1' )), name: 'Lazy 1' },
+    { to: '/lazy/2', path: 'lazy/2', component: lazy(() => import( /* webpackChunkName: "LazyPage2" */ '../01-lazyload/pages/LazyPage2' )), name: 'Lazy 2' },
+    { to: '/lazy/3', path: 'lazy/3', component: lazy(() => import( /* webpackChunkName: "LazyPage3" */ '../01-lazyload/pages/LazyPage3' )), name: 'Lazy 3' },
 ];
