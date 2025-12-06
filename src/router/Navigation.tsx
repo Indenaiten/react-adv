3cb9aaaa267875d44ed1a690cd459aebbd023de6
Ángel Herce Soto <indenaiten@gmail.com>
2025-12-06 07:15:46 +0100

    feat: se han establecido las páginas con lazyload

--- src/router/Navigation.tsx
+++ src/router/Navigation.tsx
@@ -1,36 +1,39 @@
 import { BrowserRouter, Navigate, NavLink, Route, Routes } from "react-router";
 import logo from '../logo.svg';
 import { routes } from "./routes";
+import { Suspense } from "react";
 
 export const Navigation = () => {
     return (
-        <BrowserRouter>
-            <div className="main-layout">
-                <nav>
-                    <img src={logo} alt="React Logo"/>
+        <Suspense fallback={<span>Loading...</span>}>
+            <BrowserRouter>
+                <div className="main-layout">
+                    <nav>
+                        <img src={logo} alt="React Logo"/>
 
-                    <ul>
-                        {/* Creare dinamicamente los enlaces de navegación */}
-                        { 
-                            routes.map( ({ to, name }) => (
-                                <li key={ to }>
-                                    <NavLink to={ to } className={ ({ isActive }) => isActive ? 'nav-active' : '' } end>{ name }</NavLink>
-                                </li>
+                        <ul>
+                            {/* Creare dinamicamente los enlaces de navegación */}
+                            { 
+                                routes.map( ({ to, name }) => (
+                                    <li key={ to }>
+                                        <NavLink to={ to } className={ ({ isActive }) => isActive ? 'nav-active' : '' } end>{ name }</NavLink>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </nav>
+
+                    <Routes>
+                        {/* Crear dinámicamente las rutas */}
+                        {
+                            routes.map( ({ to, path, component: Component }) => (
+                                <Route  key={ to } path={ path } element={ <Component/> }/>
                             ))
                         }
-                    </ul>
-                </nav>
-
-                <Routes>
-                    {/* Crear dinámicamente las rutas */}
-                    {
-                        routes.map( ({ to, path, component: Component }) => (
-                            <Route  key={ to } path={ path } element={ <Component/> }/>
-                        ))
-                    }
-                    <Route path="/*" element={<Navigate to={routes[0].to} replace/>}/>
-                </Routes>
-            </div>
-        </BrowserRouter>
+                        <Route path="/*" element={<Navigate to={routes[0].to} replace/>}/>
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </Suspense>
     );
 }
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,16 +1,16 @@
 import path from "path";
-import { Component, JSX } from "react";
+import { Component, JSX, lazy, LazyExoticComponent } from "react";
 import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
 
 interface Route {
     to: string,
     path: string,
-    component: () => JSX.Element,
+    component: (() => JSX.Element) | LazyExoticComponent<() => JSX.Element>,
     name: string
 }
 
 export const routes: Route[] = [
-    { to: '/lazy/1', path: 'lazy/1', component: LazyPage1, name: 'Lazy 1' },
-    { to: '/lazy/2', path: 'lazy/2', component: LazyPage2, name: 'Lazy 2' },
-    { to: '/lazy/3', path: 'lazy/3', component: LazyPage3, name: 'Lazy 3' },
+    { to: '/lazy/1', path: 'lazy/1', component: lazy(() => import( '../01-lazyload/pages/LazyPage1' )), name: 'Lazy 1' },
+    { to: '/lazy/2', path: 'lazy/2', component: lazy(() => import( '../01-lazyload/pages/LazyPage2' )), name: 'Lazy 2' },
+    { to: '/lazy/3', path: 'lazy/3', component: lazy(() => import( '../01-lazyload/pages/LazyPage3' )), name: 'Lazy 3' },
 ];
