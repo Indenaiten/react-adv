3958e911bdcae710e57d451c7f9047fffb1c4246
Ángel Herce Soto <indenaiten@gmail.com>
2025-12-08 08:28:20 +0100

    feat: se ha añadido el control del estado en el componente padre

--- src/component-patterns/hooks/ProductCounterHook.ts
+++ src/component-patterns/hooks/ProductCounterHook.ts
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { RefObject, useEffect, useRef, useState } from "react";
 import { onChangeArgs } from "../components/ProductCard";
 import { Product } from "../model";
+import { on } from "events";
 
 type args = {
     product: Product
@@ -10,18 +11,19 @@ type args = {
 
 export const useProductCounter = ({ product, onChange, value = 0 }: args ) => {
     const [ counter, setCounter ] = useState( value );
+    const isControlled: RefObject<boolean> = useRef( !!onChange );
 
-    useEffect(() => {
-        setCounter( value );
-    }, [ value ]);
+    useEffect(() => setCounter( value ), [ value ]);
     
     const increment = ( value: number ) => { 
+        if( isControlled.current ) return onChange!({ product, count: value });
         const newValue = Math.max( 0, counter + value );
         setCounter( newValue ); 
         onChange && onChange({ product, count: newValue }); 
     };
     
     const decrement = ( value: number ) => { 
+        if( isControlled.current ) return onChange!({ product, count: value * -1 });
         const newValue = Math.max( 0, counter - value );
         setCounter( newValue ); 
         onChange && onChange({ product, count: newValue }); 
--- src/component-patterns/pages/ShoppingPage.tsx
+++ src/component-patterns/pages/ShoppingPage.tsx
@@ -19,9 +19,17 @@ export const ShoppingPage = () => {
     const [shoppingCart, setShoppingCart] = useState<{ [ key: string ]: ProductCart }>({});
 
     const onProductCountChange = ({ product, count }: onChangeArgs ) => {
-        const newShoppingCart = { ...shoppingCart, [ product.id ]: { ...product, count }};
-        if( count === 0 ) delete newShoppingCart[ product.id ];
-        setShoppingCart({ ...newShoppingCart });
+        setShoppingCart( prev => {
+            const productInCart: ProductCart = shoppingCart[ product.id ] || { ...product, count: 0 };
+
+            if( Math.max( productInCart.count + count, 0 ) > 0 ){
+                productInCart.count += count;
+                return { ...shoppingCart, [ product.id ]: productInCart}
+            }
+
+            const { [ product.id ]: toRemove, ...rest } = shoppingCart;
+            return rest;
+        });
     }
 
     const getProductCount = ( product: Product ) => shoppingCart[ product.id ]?.count || 0;
