9343d26421d9668f3149bd424def3f993552af10
Ángel Herce Soto <indenaiten@gmail.com>
2025-12-08 08:52:06 +0100

    feat: se han realizado cambios para dejar la aplicación en el mismo estado del curso

--- src/component-patterns/pages/ShoppingPage.tsx
+++ src/component-patterns/pages/ShoppingPage.tsx
@@ -1,45 +1,26 @@
 import { ProductCard, ProductCardButtons, ProductCardImage, ProductCardTitle } from "../components"
 import { Product } from "../model"
 import '../styles/custom-styles.css';
-import { useShoppingCart } from "../hooks/ShoppingCartHook";
 
 const products: Product[] = [
     { id: '1', title: 'Coffee Mug - Card', img: './coffee-mug.png' },
     { id: '2', title: 'Coffe Mug - Meme', img: './coffee-mug2.png' },
 ]
 
+const product: Product = products[0];
+
 export const ShoppingPage = () => {
-    
-    const { shoppingCart, onProductCountChange, getProductCount } = useShoppingCart();
 
     return (
         <div>
             <h1>Shopping Page</h1>
             <hr />
-            
-            <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: 10 }}>
-                {
-                    products.map( product => (
-                        <ProductCard key={ product.id } product={ product } count={ getProductCount( product )} onChange={ onProductCountChange }>
-                            <ProductCardImage />
-                            <ProductCardTitle style={{ display: 'flex', justifyContent: 'center' }}/>
-                            <ProductCardButtons style={{ display: 'flex', justifyContent: 'center' }}/>
-                        </ProductCard>
-                    ))
-                }
-            </div>
 
-            <div className="shoping-cart">
-                {
-                    Object.entries( shoppingCart ).map( ( [ key, product ] ) => (
-                        <ProductCard key={key} product={ product } count={ product.count } onChange={ onProductCountChange } style={{ width: '175px' }}>
-                            <ProductCardImage />
-                            <ProductCardTitle/>
-                            <ProductCardButtons style={{ display: 'flex', justifyContent: 'center' }}/>
-                        </ProductCard>
-                    ))
-                }
-            </div>
+            <ProductCard product={ product }>
+                <ProductCardImage />
+                <ProductCardTitle style={{ display: 'flex', justifyContent: 'center' }}/>
+                <ProductCardButtons style={{ display: 'flex', justifyContent: 'center' }}/>
+            </ProductCard>
         </div>
     )
 }
